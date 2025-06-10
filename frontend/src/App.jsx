38a0f95947ef37ec445881980998c39e9986704d
kaif-lucifer <mohdkaif5243human@gmail.com>
2025-06-10 18:15:56 +0530

    Footer Error Resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import AnnouncementBar from "./components/AnnouncementBar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./Pages/Home";
 
